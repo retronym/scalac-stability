4137aabdb046422b651943c71b774fabf50b6cf8
Jason Zaugg <jzaugg@gmail.com>
2018-01-18 16:59:26 +1000

    Improve script just to take a single args file

--- scalac-stability-check.sh
+++ scalac-stability-check.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 set -e
-argsfile=$1
-sourcesfile=$2
-workingdir=$3
+argsfile=${1/@/}
+workingdir=$2
 SCALAC=${SCALAC:-scalac}
 declare -a allsources
-readarray -t allsources < $sourcesfile # Exclude newline.
 mkdir -p $workingdir
 
 refout=$workingdir/reference
@@ -14,25 +12,17 @@ mkdir -p $refout
 mkdir -p $recompileout
 recompileargsfile=$workingdir/recompileargsfile
 
-echo "Compiling with sources in given order"
-"$SCALAC" @$argsfile -d $refout @$sourcesfile
-
-# Create file with sources files in reverse order
-sourcesfile1=$workingdir/sourcesfile
-rm -rf $sourcesfile1
-for ((i=${#allsources[@]}-1; i>=0; i--)); do
-  echo "${allsources[$i]}" >> $sourcesfile1
-done
-
-echo "Recompiling with sources in reverse order"
-"$SCALAC" @$argsfile -d $recompileout @$sourcesfile1
-jardiff -r $refout $recompileout
-
-# Write a copy of the args file without `-cp\n...`
+# Write a copy of the args file without `-cp\n...` and without source files
 rm -rf $recompileargsfile
 touch $recompileargsfile
 for line in $(cat $argsfile); do
     case $line in
+    *.scala)
+        allsources+=($line)
+        ;;    
+    *.java)
+        allsources+=($line)
+        ;;
     "-classpath")
         iscp=true
         ;;
@@ -49,8 +39,24 @@ for line in $(cat $argsfile); do
     esac
 done
 
-# Recompile each source file individually
-for file in $(cat $sourcesfile); do
+echo "Compiling with sources in given order"
+"$SCALAC" -J-Xmx1G @$argsfile -d $refout
+
+# Create an argument file with sources in reverse order
+sourcesfile=$workingdir/sourcesfile
+rm -rf $sourcesfile
+touch $sourcesfile
+for ((i=${#allsources[@]}-1; i>=0; i--)); do
+  echo "${allsources[$i]}" >> $sourcesfile
+done
+
+
+echo "Recompiling with sources in reverse order"
+"$SCALAC" -J-Xmx1G @$recompileargsfile -d $recompileout @$sourcesfile
+jardiff -r $refout $recompileout
+
+for ((i=${#allsources[@]}-1; i>=0; i--)); do
+  file="${allsources[i]}"
   echo "Individually compiling $file"
   "$SCALAC" @$recompileargsfile -cp $recompileout:${classpath:-dummy} -d $recompileout $file
   jardiff -r $refout $recompileout
